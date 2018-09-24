33b539dbe360eb7a11ca3cb81c2be099fe002336
DESKTOP-UE1AEIF\marce <gicu.buruiana@gmail.com>
2018-09-24 19:04:52 -0400

    no message

--- src/components/layouts/Providers/providersListView.js
+++ src/components/layouts/Providers/providersListView.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { PanelGroup, Panel } from 'react-bootstrap';
 
 const ProvidersView = props => {
-  const { providers } = props;
-  const providerList = () => {
+  //const { providers } = props;
+
+  const providers = [{
+    name: 'grgtr',
+    path: 'sdadsa'
+  }];
+
+  const providersList = providers.map(provider => {
+    console.log('console: provider', provider);
     return (
-      Object.keys(providers).forEach(key => {
-        <div key={key}>
-          <div>name: {providers[key]}</div>
-        </div>
-      })
+        <Panel eventKey={provider.name}>
+          <Panel.Heading key={provider.name}>
+            <Panel.Title toggle>{provider.name}</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>{provider.path}</Panel.Body>
+        </Panel>
     );
-  };
+  });
 
   return (
     <div>
-      {providerList()}
+      <PanelGroup
+        accordion
+        id="accordion-controlled-example"
+        activeKey=''
+        onSelect=''
+      >
+        {providersList}
+      </PanelGroup>
     </div>
   );
 }
