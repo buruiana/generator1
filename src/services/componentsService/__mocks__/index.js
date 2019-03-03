e3cc92fb251600e0e3b901e84ef870f9d2519afc
Gicu <gicu.buruiana@gmail.com>
2019-03-03 02:09:29 -0500

    no message

--- src/services/componentsService/__mocks__/index.js
+++ src/services/componentsService/__mocks__/index.js
@@ -314,6 +314,37 @@ export const mock = {
     },
     {
       children: [],
+      componentImport: 'react-bootstrap/Container',
+      componentProps: [
+        {
+          description: 'default: <div>\t\n\nYou can use a custom element for this component',
+          name: 'as',
+          propType: 'PropTypes.element',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'default: false\t\n\nAllow the Container to fill all of it\'s availble horizontal space.',
+          name: 'fluid',
+          propType: 'PropTypes.bool',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'default: \'container\'\t\n\nChange the underlying component CSS base class name and modifier class names prefix. This is an escape hatch for working with heavily customized bootstrap css.',
+          name: 'bsPrefix',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        }
+      ],
+      description: 'Grid system#\nBootstrap’s grid system uses a series of containers, rows, and columns to layout and align content. It’s built with flexbox and is fully responsive. Below is an example and an in-depth look at how the grid comes together.\n\nNew to or unfamiliar with flexbox? Read this CSS Tricks flexbox guide for background, terminology, guidelines, and code snippets.\n\nAuto-layout columns#\nWhen no column widths are specified the Col component will render equal width columns\n\n<Container>\n  <Row>\n    <Col>1 of 2</Col>\n    <Col>2 of 2</Col>\n  </Row>\n  <Row>\n    <Col>1 of 3</Col>\n    <Col>2 of 3</Col>\n    <Col>3 of 3</Col>\n  </Row>\n</Container>;\n\nSetting one column width#\nAuto-layout for flexbox grid columns also means you can set the width of one column and have the sibling columns automatically resize around it. You may use predefined grid classes (as shown below), grid mixins, or inline widths. Note that the other columns will resize no matter the width of the center column.\n\n<Container>\n  <Row>\n    <Col>1 of 3</Col>\n    <Col xs={6}>2 of 3 (wider)</Col>\n    <Col>3 of 3</Col>\n  </Row>\n  <Row>\n    <Col>1 of 3</Col>\n    <Col xs={5}>2 of 3 (wider)</Col>\n    <Col>3 of 3</Col>\n  </Row>\n</Container>;\n\nVariable width content#\nSet the column value (for any breakpoint size) to "auto" to size columns based on the natural width of their content.\n\n<Container>\n  <Row className="justify-content-md-center">\n    <Col xs lg="2">\n      1 of 3\n    </Col>\n    <Col md="auto">Variable width content</Col>\n    <Col xs lg="2">\n      3 of 3\n    </Col>\n  </Row>\n  <Row>\n    <Col>1 of 3</Col>\n    <Col md="auto">Variable width content</Col>\n    <Col xs lg="2">\n      3 of 3\n    </Col>\n  </Row>\n</Container>;\n\nResponsive grids#\nThe Col lets you specify column widths across 5 breakpoint sizes (xs, sm, md, large, and xl). For every breakpoint, you can specify the amount of columns to span, or set the prop to <Col lg={true} /> for auto layout widths.\n\n<Container>\n  <Row>\n    <Col sm={8}>sm=8</Col>\n    <Col sm={4}>sm=4</Col>\n  </Row>\n  <Row>\n    <Col sm>sm=true</Col>\n    <Col sm>sm=true</Col>\n    <Col sm>sm=true</Col>\n  </Row>\n</Container>;\n\nYou can also mix and match breakpoints to create different grids depending on the screen size.\n\n<Container>\n  {/* Stack the columns on mobile by making one full-width and the other half-width */}\n  <Row>\n    <Col xs={12} md={8}>\n      xs=12 md=8\n    </Col>\n    <Col xs={6} md={4}>\n      xl=6 md=4\n    </Col>\n  </Row>\n\n  {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}\n  <Row>\n    <Col xs={6} md={4}>\n      xs=6 md=4\n    </Col>\n    <Col xs={6} md={4}>\n      xs=6 md=4\n    </Col>\n    <Col xs={6} md={4}>\n      xs=6 md=4\n    </Col>\n  </Row>\n\n  {/* Columns are always 50% wide, on mobile and desktop */}\n  <Row>\n    <Col xs={6}>xs=6</Col>\n    <Col xs={6}>xs=6</Col>\n  </Row>\n</Container>;\n\nThe Col breakpoint props also have a more complicated object prop form: {span: number, order: number, offset: number} for specifying offsets and ordering affects.\n\nYou can use the `order` property to control the visual order of your content.\n\n<Container>\n  <Row>\n    <Col xs>First, but unordered</Col>\n    <Col xs={{ order: 12 }}>Second, but last</Col>\n    <Col xs={{ order: 1 }}>Third, but second</Col>\n  </Row>\n</Container>;\n\nFor offsetting grid columns you can set an `offset` value, or, for more general layout, use the margin class utilities.\n\n<Container>\n  <Row>\n    <Col md={4}>md=4</Col>\n    <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>\n  </Row>\n  <Row>\n    <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>\n    <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>\n  </Row>\n  <Row>\n    <Col md={{ span: 6, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>\n  </Row>\n</Container>;',
+      id: '1Lr6DNskjxqvYJY4ksx9',
+      isDefault: true,
+      provider: 'ReactBootstrap_1.0.0-beta.5',
+      techno: 'React',
+      title: 'Container',
+      subtitle: 'ReactBootstrap_1.0.0-beta.5'
+    },
+    {
+      children: [],
       componentImport: 'react-bootstrap/ListGroup',
       componentProps: [
         {
@@ -3902,6 +3933,37 @@ export const mock = {
       subtitle: 'Bootstrap_0_32_4',
       techno: 'React',
       title: 'TabContainer'
+    },
+    {
+      children: [],
+      componentImport: 'react-bootstrap/Row',
+      componentProps: [
+        {
+          description: 'default: <div>\t\n\nYou can use a custom element type for this component.',
+          name: 'as',
+          propType: 'PropTypes.element',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'default: false\t\n\nRemoves the gutter spacing between Cols as well as any added negative margins.',
+          name: 'noGutters',
+          propType: 'PropTypes.bool',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'required\t\ndefault: \'row\'\t\n\nChange the underlying component CSS base class name and modifier class names prefix. This is an escape hatch for working with heavily customized bootstrap css.',
+          name: 'bsPrefix',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        }
+      ],
+      description: 'Row',
+      id: 'QEnkeH50ZbCXpSTk5BCG',
+      isDefault: true,
+      provider: 'ReactBootstrap_1.0.0-beta.5',
+      techno: 'React',
+      title: 'Row',
+      subtitle: 'ReactBootstrap_1.0.0-beta.5'
     },
     {
       children: [],
@@ -7569,6 +7631,61 @@ export const mock = {
       provider: 'ReactBootstrap_1.0.0-beta.5',
       techno: 'React',
       title: 'ButtonToolbar',
+      subtitle: 'ReactBootstrap_1.0.0-beta.5'
+    },
+    {
+      children: [],
+      componentImport: 'react-bootstrap/Col',
+      componentProps: [
+        {
+          description: 'default: <div>\t\n\nYou can use a custom element type for this component.',
+          name: 'as',
+          propType: 'PropTypes.element',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'type: true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }\n\nThe number of columns to span on large devices (≥992px)',
+          name: 'lg',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'type: true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }\n\nThe number of columns to span on medium devices (≥768px)',
+          name: 'md',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'type: true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }\n\nThe number of columns to span on small devices (≥576px)',
+          name: 'sm',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'type: true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }\n\nThe number of columns to span on extra large devices (≥1200px)',
+          name: 'xl',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'type: true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }\n\nThe number of columns to span on sxtra small devices (<576px)',
+          name: 'xs',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        },
+        {
+          description: 'default: \'col\'\t\n\nChange the underlying component CSS base class name and modifier class names prefix. This is an escape hatch for working with heavily customized bootstrap css.',
+          name: 'bsPrefix',
+          propType: 'PropTypes.string',
+          propTypeIsrequired: false
+        }
+      ],
+      description: 'Col',
+      id: 'mAlxTZxyHzWTCb0eSdD2',
+      isDefault: true,
+      provider: 'ReactBootstrap_1.0.0-beta.5',
+      techno: 'React',
+      title: 'Col',
       subtitle: 'ReactBootstrap_1.0.0-beta.5'
     },
     {
